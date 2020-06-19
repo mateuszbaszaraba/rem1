65163fd674a2b135e42137399110bb8240b2abb2
Matt <mateuszbaszaraba@outlook.com>
2020-06-19 13:43:14 +0200

    done header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container">
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
+
 export default {
-  data () {
-    return {
-      
-    }
+  components: {
+    appHeader: Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <router-link to='/' class="navbar-brand">StockTrader</router-link>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav mr-auto">
+
+                <router-link 
+                    to="/portfolio" 
+                    class="nav-item"
+                    activeClass='active'
+                    tag="li"><a class="nav-link">Portfolio</a></router-link>
+
+                <router-link 
+                    to='/stocks'
+                    activeClass='active'
+                    tag="li"><a class="nav-link">Stocks</a></router-link>
+
+            </ul>
+
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown" 
+                    @click="isDropdownOpen = !isDropdownOpen">
+                <a 
+                    class="nav-link dropdown-toggle" 
+                    href="#" 
+                    id="navbarDropdown" 
+                    role="button" 
+                    data-toggle="dropdown" 
+                    aria-haspopup="true" 
+                    aria-expanded="false"
+                    >Save & Load</a>
+
+                <div class="dropdown-menu" :class="{ show: isDropdownOpen }" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Save</a>
+                    <a class="dropdown-item" href="#">Load</a>
+                </div>
+            </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            isDropdownOpen: false
+        }
+    }
+}
+</script>
