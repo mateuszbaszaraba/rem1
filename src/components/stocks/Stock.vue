4d558776fb56e0522a5dae44cc82c2ffcab2441f
Matt <mateuszbaszaraba@outlook.com>
2020-06-21 20:47:34 +0200

    added cards

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <p>ID: {{stock.id}} | {{stock.name}} | {{stock.price}}</p>
+        <div class="col-sm-6 col-md-4">
+            <div class="card">
+                <h5 class="card-header">{{ stock.name }}</h5>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
